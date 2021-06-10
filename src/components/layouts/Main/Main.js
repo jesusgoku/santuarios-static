1eaf3fe8f9cafddc813bbc9b63d8e713b0df948c
Jesús Urrutia <jesus.urrutia@gmail.com>
2021-06-10 00:53:09 -0400

    feat: modify main layout

--- src/components/layouts/Main/Main.js
+++ src/components/layouts/Main/Main.js
@@ -1,11 +1,21 @@
+import Head from 'next/head';
+
 import Footer from '@app/components/presentational/Footer';
 import Header from '@app/components/presentational/Header';
 
+import styles from './Main.module.css';
+
 function MainLayout({ children }) {
   return (
-    <div class="container">
+    <div className="container">
+      <Head>
+        <title>Santuarios de Altura</title>
+        <meta name="description" content="Santuarios de Altura" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Header></Header>
-      <main>{children}</main>
+      <main className={styles.Main}>{children}</main>
       <Footer></Footer>
     </div>
   );
