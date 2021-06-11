d44fa121c5f65ec722fc14c45ffc43d4e10ef36f
Jesús Urrutia <jesus.urrutia@gmail.com>
2021-06-11 19:03:13 -0400

    feat: modify Footer and Header component

--- src/components/presentational/Footer/Footer.js
+++ src/components/presentational/Footer/Footer.js
@@ -4,32 +4,34 @@ import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <footer className={styles.Footer}>
-      <h3>Contacto</h3>
-      <ul className="list-unstyled">
-        <li>
-          Santuarios de Altura:{' '}
-          <a href="mailto:santuariosdealtura@gmail.com">
-            santuariosdealtura@gmail.com
-          </a>
-        </li>
-        <li>
-          CAPS:{' '}
-          <a href="mailto:limpiezadelplomo@gmail.com">
-            limpiezadelplomo@gmail.com
-          </a>
-        </li>
-        <li>
-          Apus Tutelares:{' '}
-          <a href="mailto:apustutelares@gmail.com">apustutelares@gmail.com</a>
-        </li>
-        <li>
-          MAC:{' '}
-          <a href="mailto:montanismoaloscolegios@gmail.com">
-            montanismoaloscolegios@gmail.com
-          </a>
-        </li>
-      </ul>
+    <footer className={`row ${styles.Footer}`}>
+      <div className="col">
+        <h3>Contacto</h3>
+        <ul className="list-unstyled">
+          <li>
+            Santuarios de Altura:{' '}
+            <a href="mailto:santuariosdealtura@gmail.com">
+              santuariosdealtura@gmail.com
+            </a>
+          </li>
+          <li>
+            CAPS:{' '}
+            <a href="mailto:limpiezadelplomo@gmail.com">
+              limpiezadelplomo@gmail.com
+            </a>
+          </li>
+          <li>
+            Apus Tutelares:{' '}
+            <a href="mailto:apustutelares@gmail.com">apustutelares@gmail.com</a>
+          </li>
+          <li>
+            MAC:{' '}
+            <a href="mailto:montanismoaloscolegios@gmail.com">
+              montanismoaloscolegios@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
--- src/components/presentational/Header/Header.js
+++ src/components/presentational/Header/Header.js
@@ -2,65 +2,68 @@ import Link from 'next/link';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 
 import styles from './Header.module.css';
 
 function Header() {
   return (
-    <Navbar expand="lg">
-      {/* <Link href="/" passHref>
+    <Navbar expand="md" sticky="top" className={styles.Navbar}>
+      <Container>
+        {/* <Link href="/" passHref>
         <Navbar.Brand>Santuarios de Altura</Navbar.Brand>
       </Link> */}
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto flex-fill justify-content-around text-uppercase">
-          <Link href="/" passHref>
-            <Nav.Link>Inicio</Nav.Link>
-          </Link>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto flex-fill justify-content-around text-uppercase">
+            <Link href="/" passHref>
+              <Nav.Link>Inicio</Nav.Link>
+            </Link>
 
-          {/* <Link href="/mision-vision" passHref>
+            {/* <Link href="/mision-vision" passHref>
             <Nav.Link>Acerca De</Nav.Link>
           </Link> */}
 
-          <NavDropdown title="Acerca De" id="basic-nav-dropdown">
-            <Link href="/acerca-de/santuarios-altura" passHref>
-              <NavDropdown.Item>Santuarios de Altura</NavDropdown.Item>
-            </Link>
+            <NavDropdown title="Acerca De" id="basic-nav-dropdown">
+              <Link href="/acerca-de/santuarios-altura" passHref>
+                <NavDropdown.Item>Santuarios de Altura</NavDropdown.Item>
+              </Link>
 
-            <Link href="/acerca-de/mision-vision" passHref>
-              <NavDropdown.Item>Misión y Visión</NavDropdown.Item>
-            </Link>
+              <Link href="/acerca-de/mision-vision" passHref>
+                <NavDropdown.Item>Misión y Visión</NavDropdown.Item>
+              </Link>
 
-            <Link href="/acerca-de/principios" passHref>
-              <NavDropdown.Item>Principios</NavDropdown.Item>
-            </Link>
+              <Link href="/acerca-de/principios" passHref>
+                <NavDropdown.Item>Principios</NavDropdown.Item>
+              </Link>
 
-            <Link href="/acerca-de/forma-trabajo" passHref>
-              <NavDropdown.Item>Forma de Trabajo</NavDropdown.Item>
-            </Link>
-          </NavDropdown>
+              <Link href="/acerca-de/forma-trabajo" passHref>
+                <NavDropdown.Item>Forma de Trabajo</NavDropdown.Item>
+              </Link>
+            </NavDropdown>
 
-          <NavDropdown title="Iniciativas" id="basic-nav-dropdown">
-            <Link href="/iniciativas/caps" passHref>
-              <NavDropdown.Item>CAPS</NavDropdown.Item>
-            </Link>
+            <NavDropdown title="Iniciativas" id="basic-nav-dropdown">
+              <Link href="/iniciativas/caps" passHref>
+                <NavDropdown.Item>CAPS</NavDropdown.Item>
+              </Link>
 
-            <Link href="/iniciativas/apus" passHref>
-              <NavDropdown.Item>Apus Tutelares</NavDropdown.Item>
-            </Link>
+              <Link href="/iniciativas/apus" passHref>
+                <NavDropdown.Item>Apus Tutelares</NavDropdown.Item>
+              </Link>
 
-            {/* <Link href="/iniciativa/mac" passHref>
+              {/* <Link href="/iniciativa/mac" passHref>
               <NavDropdown.Item>MAC</NavDropdown.Item>
             </Link> */}
-          </NavDropdown>
+            </NavDropdown>
 
-          <Link href="/contacto" passHref>
-            <Nav.Link>Contacto</Nav.Link>
-          </Link>
-        </Nav>
-      </Navbar.Collapse>
+            <Link href="/contacto" passHref>
+              <Nav.Link>Contacto</Nav.Link>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
