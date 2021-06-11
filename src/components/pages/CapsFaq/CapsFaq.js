5aebe0a8e65fd2b36bd64be2c5f95bb43ebf14fc
Jesús Urrutia <jesus.urrutia@gmail.com>
2021-06-11 19:02:22 -0400

    feat: add CapsFaq component and route

---
+++ src/components/pages/CapsFaq/CapsFaq.js
@@ -0,0 +1,190 @@
+import Head from 'next/head';
+
+import MainLayout from '@app/components/layouts/Main';
+
+function CapsFaq() {
+  return (
+    <MainLayout>
+      <Head>
+        <title>Preguntas Frecuentes - Santuarios de Altura</title>
+      </Head>
+
+      <h2>Preguntas Frecuentes</h2>
+
+      <dl>
+        <dt>¿Qué es CAPS?</dt>
+        <dd>
+          Camino al Protector de Santiago (CAPS), es una iniciativa de la
+          Comunidad Santuarios de Altura, existente desde el año 2013, que busca
+          capacitar y promocionar la conservación, cuidado y limpieza de los
+          espacios naturales y culturales en los que se desarrolla el
+          montañismo. Es un proyecto sin fines de lucro, autofinanciado.
+        </dd>
+
+        <dt>¿En qué consiste?</dt>
+        <dd>
+          <p>
+            Consiste en un “Proceso” de preparación y aprendizaje para la
+            práctica responsable del montañismo desde las perspectivas de
+            seguridad, conservación, cuidado de los espacios naturales y la
+            puesta en valor de la Cultura Andina.
+          </p>
+          <p>
+            Para ello, se organiza un nuevo “Proceso CAPS” en donde se hace un
+            llamado a Voluntarios que quieran participar de este ciclo, dura
+            alrededor de 6 meses. Comienza con la capacitación de los
+            interesados mediante charlas y talleres, para luego continuar con la
+            preparación física y técnica mediante el desarrollo de actividades
+            en terreno, que consiste en el ascenso sistemático de cerros, cuya
+            exigencia va en aumento hasta culminar con la visita y limpieza del
+            Cerro El Plomo (5.424 msnm).
+          </p>
+        </dd>
+
+        <dt>¿Qué es Santuarios de Altura?</dt>
+        <dd>
+          <p>
+            Santuarios de Altura es la organización que alberga los proyectos o
+            iniciativas: CAPS, Montañismo a los Colegios (MAC) y Apus Tutelares.
+          </p>
+          <p>
+            Santuarios de Altura es una Comunidad conformada por voluntarios de
+            diversas formaciones que valoran y respetan la diversidad de las
+            personas, no buscan reconocimientos personales y combinan el deporte
+            con el rescate de la Cultura Andina, el amor a las montañas y la
+            naturaleza. Se busca practicar Ayni (Reciprocidad) y hacer Ayllu
+            (Comunidad), como parte del propio caminar y acercamiento a la
+            Cultura Andina.
+          </p>
+          <p>
+            Formalmente, Santuarios de Altura es una organización comunitaria de
+            carácter funcional, con personalidad jurídica sin fines de lucro.
+          </p>
+        </dd>
+
+        <dt>¿Qué es una iniciativa?</dt>
+        <dd>
+          <p>
+            Iniciativa, es la forma en que llamamos a los proyectos que se
+            desarrollan en Santuarios de Altura. Actualmente existen tres
+            inicitivas activas:
+          </p>
+          <ol>
+            <li>
+              CAPS (Camino al Protector de Santiago): cuyo objetivo es promover
+              la práctica responsable del montañismo (desde las perspectivas de
+              seguridad, cuidado de los espacios naturales, valor de la Cultura
+              Andina) y crear conciencia a través de la limpieza de cerros
+              mediante el servicio y trabajo de voluntarios, culminando con la
+              limpieza de el cerro El Plomo a 5.425 msnm.
+            </li>
+            <li>
+              Montañismo a los Colegios (MAC): que lleva la cultura andina y
+              enseñanza del cuidado de la naturaleza a profesores y niños de
+              colegios, usando como principal herramienta el montañismo.
+            </li>
+            <li>
+              Apus Tutelares: cuyo principal objetivo es documentar y registrar
+              los sitios ceremoniales de altura y su historia, su misión es
+              enfatizar la importancia de los Apus Tutelares como valor
+              patrimonial y para las comunidades que poseen vínculos con ellos.
+            </li>
+          </ol>
+        </dd>
+
+        <dt>¿Qué es comunidad?</dt>
+        <dd>
+          Una Comunidad es un conjunto de personas que comparten intereses,
+          elementos y actividades en común. Por lo general, en una comunidad se
+          crea una identidad, reglas y formas de trabajo que son compartidas y
+          elaboradas por sus integrantes mediante el mutuo acuerdo.
+          Generalmente, una comunidad se une o forma, bajo la necesidad o mejora
+          de un objetivo en común.
+        </dd>
+
+        <dt>¿Qué necesito para participar?</dt>
+        <dd>
+          Tener ganas e interés por el cuidado del medio ambiente, cultura
+          andina, trekking, montañismo y el tiempo para participar en las
+          charlas y las salidas a terreno.
+        </dd>
+
+        <dt>¿Cuánto tiempo dura el proceso?</dt>
+        <dd>
+          Aproximadamente 6 meses (entre Julio de un año y Febrero del
+          siguiente).
+        </dd>
+
+        <dt>¿Tiene costo alguno?</dt>
+        <dd>No tiene costo.</dd>
+
+        <dt>¿Qué es lo básico que necesito para participar?</dt>
+        <dd>
+          Tener ganas e interés por el cuidado del medio ambiente, cultura
+          andina, trekking, montañismo y el tiempo para participar en las
+          charlas y las salidas a terreno.
+        </dd>
+
+        <dt>¿Qué días se realizan las actividades?</dt>
+        <dd>
+          Las charlas los días viernes después del horario laboral y las salidas
+          los fines de semana. Hay salidas que son de una jornada y otras con
+          campamento (más de un día).
+        </dd>
+
+        <dt>Nunca he subido un cerro ¿puedo participar?</dt>
+        <dd>
+          Si puedes. Iniciamos desde lo más básico, solamente debes poner de tu
+          parte con el esfuerzo físico, el equipamiento y el autocuidado de
+          salud física y mental.
+        </dd>
+
+        <dt>
+          ¿Qué sucede si no asisto a una salida? ¿Puedo continuar del proceso?
+        </dt>
+        <dd>
+          La continuidad del proceso por inasistencias debe evaluarse por el
+          equipo de monitores, ya que depende de factores para evaluar la
+          continuidad del proceso. Esta será informada en forma privada hacia tu
+          persona.
+        </dd>
+
+        <dt>¿Puedo ir con mi familia? ¿Pueden participar niños?</dt>
+        <dd>
+          Puede participar cualquier persona, pero el requisito es una edad
+          mínima de 18 años.
+        </dd>
+
+        <dt>¿Puedo asistir solo a las charlas?</dt>
+        <dd>
+          Sí, puedes asistir solo a las charlas incluso sin estar activo o
+          inscrito en el proceso.
+        </dd>
+
+        <dt>¿Dan un certificado al final del proceso?</dt>
+        <dd>
+          No, no entregamos ningún tipo de certificado, acreditación o
+          certificación alguna.
+        </dd>
+
+        <dt>¿Cómo puedo llegar a los cerros si no tengo transporte?</dt>
+        <dd>
+          Como grupo nos organizamos con los vehículos disponibles de monitores
+          y voluntarios para intentar cubrir los cupos de ojalá todos los
+          asistentes, pero cada voluntario o monitor es responsable de como
+          llegar al punto de encuentro. Además, cada dueño del vehículo informa
+          sus condiciones (apoyo monetario para combustibe, lugar de recogida,
+          etc.); eso ya queda a criterio personal y decisión de cada uno.
+        </dd>
+
+        <dt>¿Se puede asistir con mascotas?</dt>
+        <dd>
+          No, las mascotas no están permitidas en nuestras actividades. Esta
+          decisión por un tema de cuidado y conservación de la flora y fauna.
+        </dd>
+      </dl>
+    </MainLayout>
+  );
+}
+
+export default CapsFaq;
