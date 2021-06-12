de71e58702b734170342c30269fe4a21192264c0
Jesús Urrutia <jesus.urrutia@gmail.com>
2021-06-11 20:58:35 -0400

    feat: add more icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,10 +6,30 @@ import {
   faMountain,
   faInfoCircle,
 } from '@fortawesome/free-solid-svg-icons';
+import {
+  faInstagramSquare,
+  faFacebookSquare,
+  faYoutubeSquare,
+  faInstagram,
+  faFacebook,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 
 import '@app/styles/globals.scss';
 
-library.add(faEnvelope, faHome, faComment, faMountain, faInfoCircle);
+library.add(
+  faEnvelope,
+  faHome,
+  faComment,
+  faMountain,
+  faInfoCircle,
+  faInstagramSquare,
+  faFacebookSquare,
+  faYoutubeSquare,
+  faInstagram,
+  faFacebook,
+  faYoutube
+);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
