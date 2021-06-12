e04394e26a1fc930b93d972caa7eae5a584d727b
Jesús Urrutia <jesus.urrutia@gmail.com>
2021-06-11 20:19:24 -0400

    feat: init FontAwesome

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,15 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faEnvelope,
+  faHome,
+  faComment,
+  faMountain,
+  faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons';
+
 import '@app/styles/globals.scss';
+
+library.add(faEnvelope, faHome, faComment, faMountain, faInfoCircle);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
