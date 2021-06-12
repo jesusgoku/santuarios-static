2e0fc15b2f6a3bf05288c63bc72d1c1197913795
Jesús Urrutia <jesus.urrutia@gmail.com>
2021-06-11 20:58:56 -0400

    feat: add social networks to footer

--- src/components/presentational/Footer/Footer.js
+++ src/components/presentational/Footer/Footer.js
@@ -5,32 +5,70 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <footer className={`row ${styles.Footer}`}>
-      <div className="col">
+      <div className="col-md-6">
         <h3>Contacto</h3>
-        <ul className="list-unstyled">
-          <li>
-            <FontAwesomeIcon icon="envelope" listItem /> Santuarios de Altura:{' '}
+        <dl>
+          <dt>Santuarios de Altura:</dt>
+          <dd>
             <a href="mailto:santuariosdealtura@gmail.com">
+              <FontAwesomeIcon icon="envelope" listItem />{' '}
               santuariosdealtura@gmail.com
             </a>
-          </li>
+          </dd>
 
-          <li>
-            <FontAwesomeIcon icon="envelope" listItem /> CAPS:{' '}
+          <dt>CAPS:</dt>
+          <dd>
             <a href="mailto:limpiezadelplomo@gmail.com">
+              <FontAwesomeIcon icon="envelope" listItem />{' '}
               limpiezadelplomo@gmail.com
+            </a>
+          </dd>
+
+          <dt>Apus Tutelares:</dt>
+          <dd>
+            <a href="mailto:apustutelares@gmail.com">
+              <FontAwesomeIcon icon="envelope" listItem />{' '}
+              apustutelares@gmail.com
+            </a>
+          </dd>
+
+          <dt>MAC:</dt>
+          <dd>
+            <a href="mailto:montanismoaloscolegios@gmail.com">
+              <FontAwesomeIcon icon="envelope" listItem />{' '}
+              montanismoaloscolegios@gmail.com
+            </a>
+          </dd>
+        </dl>
+      </div>
+
+      <div className="col-md-6">
+        <h3>Redes Sociales</h3>
+        <ul className="list-unstyled">
+          <li>
+            <a
+              href="https://www.instagram.com/caps_limpieza_de_el_plomo/"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={['fab', 'instagram']} /> Instagram
             </a>
           </li>
 
           <li>
-            <FontAwesomeIcon icon="envelope" listItem /> Apus Tutelares:{' '}
-            <a href="mailto:apustutelares@gmail.com">apustutelares@gmail.com</a>
+            <a
+              href="https://www.facebook.com/Santuarios-de-Altura-344307135954852/"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={['fab', 'facebook']} /> Facebook
+            </a>
           </li>
 
           <li>
-            <FontAwesomeIcon icon="envelope" listItem /> MAC:{' '}
-            <a href="mailto:montanismoaloscolegios@gmail.com">
-              montanismoaloscolegios@gmail.com
+            <a
+              href="https://www.youtube.com/user/santuariosdealtura/"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={['fab', 'youtube']} /> Youtube
             </a>
           </li>
         </ul>
