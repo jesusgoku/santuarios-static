759630accf950a064718966605e00058d366c81b
Jesús Urrutia <jesus.urrutia@gmail.com>
2023-07-19 07:07:57 -0400

    ci: config Github Actions workflow

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   'src/**/*.js': ['prettier --write'],
+  'lint-staged.config.js': ['prettier --write'],
+  'commitlint.config.js': ['prettier --write'],
+  '.prettier.js': ['prettier --write'],
+  'jsconfig.json': ['prettier --write'],
+  '.github/**/*.yml': ['prettier --write'],
 };
